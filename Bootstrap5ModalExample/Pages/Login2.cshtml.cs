f93701a52184db69d92b6cbecf986fe69332ed05
Karen Payne <paynekaren@comcast.net>
2023-05-04 13:15:40 -0700

    more modal samples

---
+++ Bootstrap5ModalExample/Pages/Login2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bootstrap5ModalExample.Pages
+{
+    public class Login2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
