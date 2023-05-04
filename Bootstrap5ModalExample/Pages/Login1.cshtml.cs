5027928fada4b39ece622747a21678201c5e028d
Karen Payne <paynekaren@comcast.net>
2023-05-04 06:34:40 -0700

    login dialog

---
+++ Bootstrap5ModalExample/Pages/Login1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bootstrap5ModalExample.Pages
+{
+    public class Login1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
