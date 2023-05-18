d979a3fb5700d1390f7ea811a07cbfb5c6bdceae
Karen Payne <paynekaren@comcast.net>
2023-05-18 07:43:00 -0700

    added reset to progressbar

--- Bootstrap5ProgressBarExample/wwwroot/js/ClaimsProgressbar.js
+++ Bootstrap5ProgressBarExample/wwwroot/js/ClaimsProgressbar.js
@@ -1,9 +1,12 @@
 ﻿var $ClaimsProgressbar = $ClaimsProgressbar || {};
 $ClaimsProgressbar = function () {
+
     var progressWord = "Progress";
+    var parentContainer;
 
     var init = function (option) {
-        
+        parentContainer = option;
+        console.log(parentContainer);
     };
 
     //
@@ -41,12 +44,43 @@ $ClaimsProgressbar = function () {
         return parseInt($(identifier).attr("aria-valuenow"));
     };
 
+    /*
+     * Hide progressbar, set to 0, show progressbar
+     */
     var reset = function(identifier) {
 
-        console.log('dsss');
-        $('#progressStatus').hide();
-        $('#progressStatus').show();
+        toggle(document.querySelectorAll('.progress'));
+        increment(identifier, 0);
+        toggle(document.querySelectorAll('.progress'));
     };
+
+    /*
+     * Toggle element visibility
+     */
+    function toggle(elements, specifiedDisplay) {
+        var element, index;
+
+        elements = elements.length ? elements : [elements];
+        for (index = 0; index < elements.length; index++) {
+            element = elements[index];
+
+            if (isElementHidden(element)) {
+                element.style.display = '';
+
+                // If the element is still hidden after removing the inline display
+                if (isElementHidden(element)) {
+                    element.style.display = specifiedDisplay || 'block';
+                }
+            } else {
+                element.style.display = 'none';
+            }
+        }
+
+        function isElementHidden(element) {
+            return window.getComputedStyle(element, null).getPropertyValue('display') === 'none';
+        }
+
+    }
     //
     // Show progressbar by id e.g.
     //    $ClaimsProgressbar.Show('#progressStatus')
@@ -54,7 +88,6 @@ $ClaimsProgressbar = function () {
     var show = function (identifier) {
         $(assertPoundSymbol(identifier)).show();
     };
-
 
     //
     // Hide progressbar by id e.g.
@@ -71,7 +104,6 @@ $ClaimsProgressbar = function () {
         Show: show,
         Hide: hide,
         reset: reset
-
     };
 }();
 
