85717c22a9f09b0607831491434c524ec3874582
Karen Payne <paynekaren@comcast.net>
2023-08-01 10:17:18 -0700

    various changes and new project

--- BootstrapInputGroupValidateExample/Pages/Index.cshtml.cs
+++ BootstrapInputGroupValidateExample/Pages/Index.cshtml.cs
@@ -15,11 +15,6 @@ public class IndexModel : PageModel
     [BindProperty]
     public string LastName { get; set; }
 
-    public IndexModel()
-    {
-        
-    }
-
     public void OnGet()
     {
         Person = new Person();
