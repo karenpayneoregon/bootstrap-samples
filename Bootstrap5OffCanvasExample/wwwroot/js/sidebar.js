1f5ac133d69b8c329cf70035192181e4dfc0e77b
Karen Payne <paynekaren@comcast.net>
2023-11-27 07:29:17 -0800

    Fix off-canvas synchronization issue and add skip link

--- Bootstrap5OffCanvasExample/wwwroot/js/sidebar.js
+++ Bootstrap5OffCanvasExample/wwwroot/js/sidebar.js
@@ -2,10 +2,9 @@
 var offcanvas_el = document.getElementById("offcanvasMain");
 var offcanvas = new bootstrap.Offcanvas(offcanvas_el, { backdrop: false });
 
-toggle.addEventListener("change",
-    function () {
-        toggle.checked ? offcanvas.show() : offcanvas.hide();
-    });
+toggle.addEventListener("change", function () {
+    toggle.checked ? offcanvas.show() : offcanvas.hide();
+});
 
 // handle case when sidebar is closed internally using `X`
 offcanvas_el.addEventListener('hide.bs.offcanvas', function () {
