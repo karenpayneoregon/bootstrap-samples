a921024d3e2367b0c57c18d5a239900d47cd5e62
Karen Payne <paynekaren@comcast.net>
2024-03-29 10:23:05 -0700

    updates

--- LoginFormsApp/Pages/Feathers1.cshtml.cs
+++ LoginFormsApp/Pages/Feathers1.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LoginFormsApp.Pages
 {
-    public class Login3Model : PageModel
+    public class Feathers1Model : PageModel
     {
         public void OnGet()
         {
