2c7d071f33c656d9347eb8f80cd540453b44b7c8
Karen Payne <paynekaren@comcast.net>
2024-03-29 14:05:26 -0700

    updates

---
+++ LoginFormsApp/Pages/LoginWithEye.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginFormsApp.Pages
+{
+    public class LoginWithEyeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
