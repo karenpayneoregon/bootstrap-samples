80248b9bc6ba26f790a1df468c0a29d54b80d468
Karen Payne <paynekaren@comcast.net>
2024-05-24 01:35:55 -0700

    `Updated event handlers, project framework, and added new project`
    Updated jQuery event handlers to vanilla JavaScript in `Login1.cshtml` and made formatting changes. Upgraded the target framework from `net7.0` to `net8.0` in `Bootstrap5PopoverExample.csproj`. Reformatted `MockedData.cs` for better readability. Changed alert message and button text in `Index.cshtml`. Commented out navbar brand link in `_Layout.cshtml`. Added new project `ConsoleApp1` to `BootstrapSolution.sln` and changed project type of `NavigationUnderlineSample`. Created new files `ConsoleApp1.csproj` and `Program.cs` for `ConsoleApp1` project, a console application targeting .NET 8.0.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿    using System.Text.RegularExpressions;
+
+    namespace ConsoleApp1;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "\"Hello, World!\"";
+            string result = RemoveQuotes(input);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static string RemoveQuotes(string input)
+        {
+            string result = Regex.Replace(input, "^\"|\"$", "");
+            return result;
+        }
+    }
+
+
+
