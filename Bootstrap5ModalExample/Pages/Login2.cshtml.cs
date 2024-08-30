13bac8630614026399156fe02839d3d39587e2ee
Karen Payne <paynekaren@comcast.net>
2024-08-30 02:39:15 -0700

    Add settings page, enhance login form, update styles
    Introduced a new settings page for dark mode and personal info.
    Enhanced the login form with hidden fields and submission logic.
    Updated styles and scripts to support these changes.
    Removed dark mode handling from the index page.

--- Bootstrap5ModalExample/Pages/Login2.cshtml.cs
+++ Bootstrap5ModalExample/Pages/Login2.cshtml.cs
@@ -3,10 +3,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Bootstrap5ModalExample.Pages
 {
+    
     public class Login2Model : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public int Pin { get; set; }
+        [BindProperty]
+        public string UserName { get; set; }
+
+        public void OnGet() { }
+
+        public IActionResult OnPost()
         {
+            var name = UserName;
+            if (Pin == 1234)
+            {
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
---
+++ DarkLightMode/Pages/SettingsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DarkLightMode.Pages
+{
+    public class SettingsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
